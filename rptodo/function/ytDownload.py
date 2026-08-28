92bc9845f292b2c25144e8daaab3cde15c5cbba4
alief wafiq ramadhan <aliefwafiq2@gmail.com>
2026-08-28 16:59:14 +0700

    first

---
+++ rptodo/function/ytDownload.py
@@ -0,0 +1,45 @@
+import yt_dlp
+import typer
+import sys
+
+def proggres(stream, chunk, bytesRemaining):
+    totalSize = stream.filesize
+    bytesDownloaded = totalSize - bytesRemaining
+    percentage = (bytesDownloaded / totalSize) * 100
+
+    sys.stdout.write(f"Downloading... {percentage:.2f}%")
+    sys.stdout.flush()
+
+def ytDownload(url, type):
+    try:
+        if type == "video":
+            ydl_opts = {
+                "format": "bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best",
+                "outtmpl": "Downloads/%(title)s.%(ext)s",
+            }
+
+        else:
+            ydl_opts = {
+                "format": "bestaudio/best",
+                "outtmpl": "Downloads/%(title)s.%(ext)s",
+                "postprocessors": [{
+                    "key": "FFmpegExtractAudio",
+                    "preferredcodec": "mp3",
+                    "preferredquality": "192"
+                }]
+            }
+
+        typer.secho(f"Memulai download...", fg=typer.colors.YELLOW)
+
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            info = ydl.extract_info(url, download=False)
+            title = info.get("title", "Unknown Title")
+            typer.secho(f"Judul: {title}\n", fg=typer.colors.CYAN, bold=True)
+
+            ydl.download([url])
+
+        typer.secho(f"\nDownload selesai", fg=typer.colors.GREEN, bold=True)
+
+    except Exception as e:
+        typer.secho(f"\n Terjadi kesalahan {e}", fg=typer.colors.RED)
+
